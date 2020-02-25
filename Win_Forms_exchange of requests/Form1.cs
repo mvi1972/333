f7c23c45bc6d65ffc32b9593cd92eb7a864b638a
vv <4590955@list.ru>
2020-02-25 19:15:03 +0300

    01

---
+++ Win_Forms_exchange of requests/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Win_Forms_exchange_of_requests
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("http://www.professorweb.ru");
+            StreamReader sr = new StreamReader(stream);
+            string newLine;
+            while ((newLine = sr.ReadLine()) != null)
+                textBox1.Text += newLine;
+
+            stream.Close();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
